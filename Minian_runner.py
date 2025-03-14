4cbc181b0367b61e17d5fff1b27eb33cca198f0d
Axelle Piguet <44366648+axpiguet@users.noreply.github.com>
2025-03-14 12:15:23 +0100

    runner update

--- Minian_runner.py
+++ Minian_runner.py
@@ -17,6 +17,7 @@ if sys.platform.startswith("win"):
 
 # Default notebook path
 DEFAULT_NOTEBOOK_PATH = "C:/Users/axelle.piguet/MiniscopeAnimalParam.ipynb"
+DEFAULT_CROSSREG_PATH = "C:/Users/axelle.piguet/cross-registration.ipynb"  # to fixxxxxxx
 
 def run_notebook(notebook_path, session):
     """Run a Jupyter notebook with a given session."""
@@ -26,6 +27,29 @@ def run_notebook(notebook_path, session):
     
     # Set the session path as an environment variable
     os.environ["SESSION_PATH"] = session or ""
+    
+    # Suppress output
+    output_buffer = io.StringIO()
+    try:
+        with redirect_stdout(output_buffer), redirect_stderr(output_buffer):
+            with open(notebook_path, "r", encoding="utf-8") as f:
+                notebook = nbformat.read(f, as_version=4)
+            ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+            ep.preprocess(notebook, {'metadata': {'path': notebook_path.parent}})
+        
+        # Print your custom colored message
+        print(colored("Notebook executed successfully." , 'green'))
+    except Exception as e:
+        print(colored(f"Error during execution: {e}" , 'red'))
+
+def run_crossreg(notebook_path, mouse):
+    """Run a Jupyter notebook with a given session."""
+    notebook_path = Path(notebook_path)
+    if not notebook_path.exists():
+        raise FileNotFoundError(f"Notebook {notebook_path} does not exist.")
+    
+    # Set the session path as an environment variable
+    os.environ["ANIMAL_PATH"] = mouse or ""
     
     # Suppress output
     output_buffer = io.StringIO()
@@ -54,7 +78,12 @@ if __name__ == "__main__":
         help="Run the notebook and print outputs."
     )
     parser.add_argument(
-        "--open", 
+        "--cross_reg", 
+        action="store_true", 
+        help="Open the notebook in Jupyter Notebook."
+    )
+    parser.add_argument(
+        "--tuning", 
         action="store_true", 
         help="Open the notebook in Jupyter Notebook."
     )
@@ -64,17 +93,24 @@ if __name__ == "__main__":
         default="", 
         help="Provide the path to the session."
     )
+    parser.add_argument(
+        "--mouse", 
+        type=str, 
+        default="", 
+        help="Provide the path to the animal folder."
+    )
     
     args = parser.parse_args()
     
     # Use default notebook path
     notebook_path = DEFAULT_NOTEBOOK_PATH
+    cross_reg_path = DEFAULT_CROSSREG_PATH
     
     if args.run:
         run_notebook(notebook_path, args.session)
-    elif args.open:
+    elif args.tuning:
         open_notebook(notebook_path)
-    elif args.open:
-        open_notebook(notebook_path)
+    elif args.cross_reg:
+        run_crossreg(cross_reg_path, args.mouse)
     else:
-        print("Please specify : --run to execute | --open or --tuning to open the notebook.")
+        print("Please specify : --run to execute | --tuning to open the notebook.")
